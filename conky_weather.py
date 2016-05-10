c9591a45dfabd480deaa53207800893c8487f45e
Brian Pedersen <xirov@xirov.com>
2016-05-10 15:16:13 +0200

    Added forecast feature, possible to see estimate of wind speed today, and estimates for the next 3 days

--- conky_weather.py
+++ conky_weather.py
@@ -5,22 +5,27 @@ from wu_api import WundergroundAPI
 
 ##### Edit here #####
 wu_dir = '/home/xirov/projects/conky-weather-wu'
-api_key = ''
+api_key = 'b2fd9cf3d2750fc7'
 country = 'Denmark'
 city = 'Odense'
+update_interval = 10     # in minutes
 #####################
 
 
-cur_time = time.time()
+# Check when data was last pulled
+cur_time = int(time.time())
 with open(os.path.join(wu_dir, 'last_pull'), 'r') as last_pull_f:
     try:
-        last_pull = float(last_pull_f.read().strip())
+        last_pull = int(last_pull_f.read().strip())
     except ValueError:
-        last_pull = 0.0
+        last_pull = 0
     last_pull_f.close()
 
-if last_pull + 600 < cur_time:
+update_interval_sec = update_interval * 60
 
+# If data was last pulled longer than the specified update interval
+if last_pull + update_interval_sec < cur_time:
+    # Update 'last_pull' file (containing timestamp of last pull)
     with open(os.path.join(wu_dir, 'last_pull'), 'w') as last_pull_f:
         last_pull_f.write(str(cur_time))
         last_pull_f.close()
@@ -28,59 +33,104 @@ if last_pull + 600 < cur_time:
     # Pull new data
     wuapi = WundergroundAPI(wu_dir, api_key, country, city)
     wuapi.pull()
+    print('Pulled new data')
 
+# Open wu_conditions file, and extract some data
 with open(os.path.join(wu_dir, 'wu_conditions.json'), 'r') as cond_f:
-    data = json.load(cond_f)
-    #print(json.dumps(data, sort_keys=True, indent=4))
-    location = data['location']['city']
-    temp_c = data['current_observation']['temp_c']
-    wind_kph = data['current_observation']['wind_kph']
-    weather = data['current_observation']['weather']
-    wind_dir = data['current_observation']['wind_dir']
+    cond = json.load(cond_f)
+    location = cond['location']['city']
+    temp_c = cond['current_observation']['temp_c']
+    wind_kph = cond['current_observation']['wind_kph']
+    weather = cond['current_observation']['weather']
+    wind_dir = cond['current_observation']['wind_dir']
+    cur_weather_icon = cond['current_observation']['icon']
     cond_f.close()
 
-    with open(os.path.join(wu_dir, 'wu_astronomy.json'), 'r') as astr_f:
-        astr = json.load(astr_f)
-        sunset_hour = astr['sun_phase']['sunset']['hour']
-        sunrise_hour = astr['sun_phase']['sunrise']['hour']
-        sunset_minute = astr['sun_phase']['sunset']['minute']
-        sunrise_minute = astr['sun_phase']['sunrise']['hour']
-        #print(astr['moon_phase'])
-        astr_f.close()
+# Open wu_astronomy file, and extract some data
+with open(os.path.join(wu_dir, 'wu_astronomy.json'), 'r') as astr_f:
+    astr = json.load(astr_f)
+    sunset_hour = astr['sun_phase']['sunset']['hour']
+    sunrise_hour = astr['sun_phase']['sunrise']['hour']
+    sunset_minute = astr['sun_phase']['sunset']['minute']
+    sunrise_minute = astr['sun_phase']['sunrise']['hour']
+    astr_f.close()
+
+# Open wu_forecast file, and extract some data
+with open(os.path.join(wu_dir, 'wu_forecast.json'), 'r') as forc_f:
+    forc = json.load(forc_f)
+    #print(json.dumps(forc['forecast']['simpleforecast']['forecastday'][0], indent=4))
+    day0 = forc['forecast']['simpleforecast']['forecastday'][0]
+    day1 = forc['forecast']['simpleforecast']['forecastday'][1]
+    day2 = forc['forecast']['simpleforecast']['forecastday'][2]
+    day3 = forc['forecast']['simpleforecast']['forecastday'][3]
+
+    pred_rain_today = day0['qpf_allday']['mm']
+    pred_rain_1 = day1['qpf_allday']['mm']
+    pred_rain_2 = day2['qpf_allday']['mm']
+    pred_rain_3 = day3['qpf_allday']['mm']
+
+    avewind_today = day0['avewind']['kph']
+    avewind_1 = day1['avewind']['kph']
+    avewind_2 = day2['avewind']['kph']
+    avewind_3 = day3['avewind']['kph']
+
+    icon1 = day1['icon']
+    icon2 = day2['icon']
+    icon3 = day3['icon']
+
+    temp1_low = day1['low']['celsius']
+    temp2_low = day2['low']['celsius']
+    temp3_low = day3['low']['celsius']
+    temp1_high = day1['high']['celsius']
+    temp2_high = day2['high']['celsius']
+    temp3_high = day3['high']['celsius']
+
+    day1_name = day1['date']['weekday_short']
+    day2_name = day2['date']['weekday_short']
+    day3_name = day3['date']['weekday_short']
+
 
 current_hour = time.strftime('%H')
 
-icons_day = ['', '', '', '', '', '', '', '', '']
-icons_night = ['', '', '', '', '', '', '', '', '']
+icons = {
+    'chanceflurries':'',
+    'chancerain':'',
+    'chancesleet':'',
+    'chancesnow':'',
+    'chancetstorms':'',
+    'clear':'',
+    'cloudy':'',
+    'flurries':'',
+    'fog':'',
+    'hazy':'',
+    'mostlycloudy':'',
+    'mostlysunny':'',
+    'partlycloudy':'',
+    'partlysunny':'',
+    'sleet':'',
+    'rain':'',
+    'snow':'',
+    'sunny':'',
+    'tstorms':'',
+    'nt_chanceflurries':'',
+    'nt_chancerain':'',
+    'nt_chancesleet':'',
+    'nt_chancesnow':'',
+    'nt_chancetstorms':'',
+    'nt_clear':'',
+    'nt_cloudy':'',
+    'nt_flurries':'',
+    'nt_fog':'',
+    'nt_hazy':'',
+    'nt_mostlycloudy':'',
+    'nt_mostlysunny':'',
+    'nt_partlycloudy':'',
+    'nt_partlysunny':'',
+    'nt_sleet':'',
+    'nt_rain':'',
+    'nt_snow':'',
+    'nt_sunny':'',
+    'nt_tstorms':''
+}
 
-if current_hour > sunset_hour and current_hour < sunrise_hour:
-    icons = icons_night
-else:
-    icons = icons_day
-
-if weather == 'Clear':
-    icon = icons[0]
-elif weather == 'Scattered Clouds':
-    icon = icons[1]
-elif weather == 'Partly Cloudy':
-    icon = icons[1]
-elif weather == 'Mostly Cloudy':
-    icon = icons[2]
-elif weather == 'Chance of Rain':
-    icon = icons[3]
-elif weather == 'Overcast':
-    icon = icons[4]
-elif weather == 'Rain':
-    icon = icons[5]
-elif weather == 'Snow':
-    icon = icons[6]
-elif weather == 'Fog':
-    icon = icons[7]
-elif weather == 'Thunderstorms with Hail':
-    icon = icons[8]
-elif weather == 'Tstorm':
-    icon = icons[8]
-else:
-    icon = weather
-
-print('%s %d     %d %s      %s ~ %s ' % (icon, temp_c, wind_kph, wind_dir, sunrise_hour, sunset_hour))
+print('%s %d |  %s mm |  %d %s | est  %d |  %s |  %s | %s %s %s-%s/%smm | %s %s %s-%s/%smm | %s %s %s-%s/%smm' % (icons[cur_weather_icon], temp_c, pred_rain_today, wind_kph, wind_dir, avewind_today, sunrise_hour, sunset_hour, day1_name, icons[icon1], temp1_low, temp1_high, pred_rain_1, day2_name, icons[icon2], temp2_low, temp2_high, pred_rain_2, day3_name, icons[icon3], temp3_low, temp3_high, pred_rain_3))
--- wu_api.py
+++ wu_api.py
@@ -3,19 +3,32 @@ import json
 import os
 
 class WundergroundAPI(object):
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, local_path, api_key, country, city):
+        self.local_path = local_path
+        self.api_key = api_key
+        self.country = country
+        self.city = city
 
+    # Pull data from Wunderground
     def pull(self):
-        with urllib.request.urlopen('http://api.wunderground.com/api/b2fd9cf3d2750fc7/geolookup/conditions/q/Denmark/Odense.json') as c:
-            # Save data
-            with open(os.path.join(self.path, 'wu_conditions.json'), 'w') as conditions_file:
+        with urllib.request.urlopen('http://api.wunderground.com/api/' + self.api_key + '/geolookup/conditions/q/' + self.country + '/' + self.city + '.json') as c:
+            # Save weather condition data
+            with open(os.path.join(self.local_path, 'wu_conditions.json'), 'w') as conditions_file:
                 conditions_file.write(c.read().decode('utf-8'))
                 conditions_file.close()
             c.close()
 
-        with urllib.request.urlopen('http://api.wunderground.com/api/b2fd9cf3d2750fc7/geolookup/astronomy/q/Denmark/Odense.json') as a:
-            with open(os.path.join(self.path, 'wu_astronomy.json'), 'w') as astronomy_file:
+        with urllib.request.urlopen('http://api.wunderground.com/api/' + self.api_key + '/geolookup/astronomy/q/' + self.country + '/' + self.city + '.json') as a:
+            # Save astronomy data
+            with open(os.path.join(self.local_path, 'wu_astronomy.json'), 'w') as astronomy_file:
                 astronomy_file.write(a.read().decode('utf-8'))
                 astronomy_file.close()
             a.close()
+
+        with urllib.request.urlopen('http://api.wunderground.com/api/' + self.api_key + '/geolookup/forecast/q/' + self.country + '/' + self.city + '.json') as f:
+            # Save astronomy data
+            with open(os.path.join(self.local_path, 'wu_forecast.json'), 'w') as forecast_file:
+                forecast_file.write(f.read().decode('utf-8'))
+                forecast_file.close()
+            f.close()
+
